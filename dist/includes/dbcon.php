e9ebef1ad9b4c4674302b57f73558ba5a6718c3a
Kolichi <Kolichitutu110@gmail.com>
2023-12-20 09:20:11 +0200

    Test

--- dist/includes/dbcon.php
+++ dist/includes/dbcon.php
@@ -1,8 +1,6 @@
 <?php
- 
   $con = mysqli_connect("localhost","root","","moringa_db_");
   
-// Check connection
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
